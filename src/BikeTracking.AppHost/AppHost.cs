5ac90d0ab289a68eaff3c11df0a55590deb4af95
aligneddev <aligneddev@github.com>
2026-03-12 08:53:47 -0500

    Need AddNodeApp for Aurelia

--- src/BikeTracking.AppHost/AppHost.cs
+++ src/BikeTracking.AppHost/AppHost.cs
@@ -1,10 +1,16 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.BikeTracking_Api>("api")
+var apiService = builder
+    .AddProject<Projects.BikeTracking_Api>("api")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
-var webFrontend = builder.AddViteApp("frontend", "../BikeTracking.Frontend")
+// AddViteApp was not working with Aurelia
+var webFrontend = builder
+    .AddNodeApp("frontend", "../BikeTracking.Frontend", "node_modules/vite/bin/vite.js")
+    .WithNpm()
+    .WithRunScript("preview")
+    .WithHttpEndpoint(port: 5173, env: "PORT")
     .WithReference(apiService)
     .WaitFor(apiService);
 
